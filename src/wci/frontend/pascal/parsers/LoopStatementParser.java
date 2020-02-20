89013281e11a2c0632fc36435dd27236351bcb53
Tyler <twa16@case.edu>
2020-02-20 00:49:14 -0500

    LoopStatementParser added

--- src/wci/frontend/pascal/parsers/LoopStatementParser.java
+++ src/wci/frontend/pascal/parsers/LoopStatementParser.java
@@ -38,7 +38,7 @@ public class LoopStatementParser extends StatementParser
         TO_DOWNTO_SET.add(DOWNTO);
         TO_DOWNTO_SET.addAll(StatementParser.STMT_FOLLOW_SET);
     }
-
+    
     // Synchronization set for DO.
     private static final EnumSet<PascalTokenType> DO_SET =
         StatementParser.STMT_START_SET.clone();
@@ -48,7 +48,7 @@ public class LoopStatementParser extends StatementParser
     }
 
     /**
-     * Parse the FOR statement.
+     * Parse the Loop statement.
      * @param token the initial token.
      * @return the root node of the generated parse tree.
      * @throws Exception if an error occurred.
@@ -56,7 +56,10 @@ public class LoopStatementParser extends StatementParser
     public ICodeNode parse(Token token)
         throws Exception
     {
-        token = nextToken();  // consume the FOR
+        token = nextToken();  // consume the Loop
+        
+        token = nextToken();  // consume the (
+        
         Token targetToken = token;
 
         // Create the loop COMPOUND, LOOP, and TEST nodes.
@@ -69,6 +72,8 @@ public class LoopStatementParser extends StatementParser
             new AssignmentStatementParser(this);
         ICodeNode initAssignNode = assignmentParser.parse(token);
 
+        token = nextToken();  // consume the |
+        
         // Set the current line number attribute.
         setLineNumber(initAssignNode, targetToken);
 
@@ -77,38 +82,29 @@ public class LoopStatementParser extends StatementParser
         compoundNode.addChild(initAssignNode);
         compoundNode.addChild(loopNode);
 
-        // Synchronize at the TO or DOWNTO.
-        token = synchronize(TO_DOWNTO_SET);
-        TokenType direction = token.getType();
-
-        // Look for the TO or DOWNTO.
-        if ((direction == TO) || (direction == DOWNTO)) {
-            token = nextToken();  // consume the TO or DOWNTO
-        }
-        else {
-            direction = TO;
-            errorHandler.flag(token, MISSING_TO_DOWNTO, this);
-        }
-
-        // Create a relational operator node: GT for TO, or LT for DOWNTO.
-        ICodeNode relOpNode = ICodeFactory.createICodeNode(direction == TO
-                                                           ? GT : LT);
-
-        // Copy the control VARIABLE node. The relational operator
-        // node adopts the copied VARIABLE node as its first child.
-        ICodeNode controlVarNode = initAssignNode.getChildren().get(0);
-        relOpNode.addChild(controlVarNode.copy());
-
-        // Parse the termination expression. The relational operator node
-        // adopts the expression as its second child.
+        // parse the loop condition
         ExpressionParser expressionParser = new ExpressionParser(this);
-        relOpNode.addChild(expressionParser.parse(token));
-
+        ICodeNode expressionNode = expressionParser.parse(token);
+        
         // The TEST node adopts the relational operator node as its only child.
         // The LOOP node adopts the TEST node as its first child.
-        testNode.addChild(relOpNode);
+        testNode.addChild(expressionNode);
         loopNode.addChild(testNode);
 
+        // second identifier := expression
+        token = nextToken();  // consume the |
+        
+        AssignmentStatementParser assignmentParser2 =
+        		new AssignmentStatementParser(this);
+        ICodeNode initAssignNode2 = assignmentParser2.parse(token);
+        
+        loopNode.addChild(initAssignNode2);
+
+        // Set the current line number attribute.
+        setLineNumber(initAssignNode2, targetToken);
+        
+        token = nextToken();  // consume the )
+        
         // Synchronize at the DO.
         token = synchronize(DO_SET);
         if (token.getType() == DO) {
@@ -122,33 +118,7 @@ public class LoopStatementParser extends StatementParser
         // node as its second child.
         StatementParser statementParser = new StatementParser(this);
         loopNode.addChild(statementParser.parse(token));
-
-        // Create an assignment with a copy of the control variable
-        // to advance the value of the variable.
-        ICodeNode nextAssignNode = ICodeFactory.createICodeNode(ASSIGN);
-        nextAssignNode.addChild(controlVarNode.copy());
-
-        // Create the arithmetic operator node:
-        // ADD for TO, or SUBTRACT for DOWNTO.
-        ICodeNode arithOpNode = ICodeFactory.createICodeNode(direction == TO
-                                                             ? ADD : SUBTRACT);
-
-        // The operator node adopts a copy of the loop variable as its
-        // first child and the value 1 as its second child.
-        arithOpNode.addChild(controlVarNode.copy());
-        ICodeNode oneNode = ICodeFactory.createICodeNode(INTEGER_CONSTANT);
-        oneNode.setAttribute(VALUE, 1);
-        arithOpNode.addChild(oneNode);
-
-        // The next ASSIGN node adopts the arithmetic operator node as its
-        // second child. The loop node adopts the next ASSIGN node as its
-        // third child.
-        nextAssignNode.addChild(arithOpNode);
-        loopNode.addChild(nextAssignNode);
-
-        // Set the current line number attribute.
-        setLineNumber(nextAssignNode, targetToken);
-
+        
         return compoundNode;
     }
 }
